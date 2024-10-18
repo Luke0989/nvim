16fa534f0631fc95495b83a954ef18c3d374687d
Luke-G91 <luke.goodwin@ada.ac.uk>
2024-10-18 23:33:14 +0100

    update colour scheme

--- lua/config/plugins/colourscheme.lua
+++ lua/config/plugins/colourscheme.lua
@@ -1,11 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-    })
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "storm",
+			terminal_colors = true,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = { bold = true },
+				variables = {},
+			},
+			sidebars = { "qf", "help" },
+			on_highlights = function(hl, c)
+				hl.CursorLine = { bg = c.bg_dark }
+				hl.Normal = { bg = c.bg_dark }
+			end,
+		})
 
-    vim.cmd("colorscheme tokyonight")
-  end
+		vim.cmd("colorscheme tokyonight")
+	end,
 }
